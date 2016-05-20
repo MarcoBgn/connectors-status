033c6d64c449dc17e3f5f9555fd3a17d1a0f4eee
Marco Bagnasco <admin@hecticmojo.com>
2016-05-20 16:54:36 +0100

    Added unit test for UserService using

--- test/spec/services/userServiceSpec.js
+++ test/spec/services/userServiceSpec.js
@@ -2,14 +2,24 @@
 
 describe('Service: ListRepos', function () {
     
-  var ListRepos, httpBackEnd;
+  var ListRepos, httpBackend, url;
     beforeEach(module('gitProfileApp'));
-    beforeEach(inject(function ($controller, _ListRepos_, $httpBackEnd) {
+    beforeEach(inject(function ($controller, _ListRepos_, $httpBackend) {
       ListRepos = _ListRepos_;
-      httpBackEnd = $httpBackEnd;
+      httpBackend = $httpBackend;
+      url = 'https://api.github.com/users/marcocode/repos'
     }));
     
     it('parses a Url and retrieves a list of repos', function () {
-      expect(ListRepos.parseUrl('https://api.github.com/users/MarcoCode/repos')).toBeDefined();
+      expect(ListRepos.parseUrl('localhost')).toBeDefined();
+    })
+    
+    it('returns a list of repos for the user or org', function () {
+      httpBackend.expectGET(url).respond(function () {
+        return [200, 'response body', {repo1: {name: "test", language: "Ruby"}}];
+      })
+      ListRepos.parseUrl(url).then(function (response) {
+        expect(response.data.repo1.name).toEqual("test");
+      });
     })
 })
