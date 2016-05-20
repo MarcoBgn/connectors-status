9564927d58786d6091343ba3d4dc41f6a2338317
Marco Bagnasco <admin@hecticmojo.com>
2016-05-20 16:21:32 +0100

    Added additional  integration tests for main page

--- test/e2e/mainPageSpec.js
+++ test/e2e/mainPageSpec.js
@@ -12,12 +12,26 @@ describe('gitProfile', function () {
   
     it('Prompts the user with a description', function () {
       var main = $('body');
-      expect(main.getText()).toContain('Select user or organisation');
+      expect(main.getText()).toContain('Type a GitHub user or organisation');
     });
     
-    it('Displays a form', function () {
+    it('Displays a form field', function () {
       var form = element(by.id('profile_form'));
       expect(form.isDisplayed()).toBe(true);
+    })
+    
+    it('Displays a search button', function () {
+      var button = element(by.id('search_button'));
+      expect(button.isDisplayed()).toBe(true);
+    })
+    
+    it('Allow to search with a username or company name', function () {
+      var form = element(by.id('search_form'));
+      var button = element(by.id('search_button'));
+      var main = $('body');  
+      form.sendKeys('marcocode');
+      button.click();
+      expect(main.getText()).toContain('airport_challenge');
     })
   })
 })
